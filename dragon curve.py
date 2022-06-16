7a141205643c64c51ac016bda370ab58e7e6de7f
The Stanley Parable Adventure Line <105490450+TheStanleyParableAdventureLine@users.noreply.github.com>
2022-06-16 22:01:46 +0100

    Update dragon curve.py

--- dragon curve.py
+++ dragon curve.py
@@ -3,6 +3,7 @@
 
 from turtle import *
 
+# setting up the window (wn) and the turtle for drawing the curve
 wn = Screen()
 pen = Turtle()
 pen.speed(0)
@@ -20,7 +21,7 @@ def mid(a, b):
 #dragon curve function
 def dragon(p1, p2, order, flip):
     
-    #check if we need to iterate
+    #check if we can draw a line yet
     if order > 0:
         
         #calculate midpoint of p1 and p2
@@ -28,24 +29,25 @@ def dragon(p1, p2, order, flip):
                 mid(p1[1], p2[1])
                 ]
 
-        if flip == False:            
+        #basic maths to find where the next point to draw to/from is
+        #to get a better idea for how this works, see this graph: https://www.desmos.com/calculator/ccmf3rhlnp
+        if flip == False:
             newPoint = [(midP[0] - (midP[1] - p1[1])),
                         (midP[1] + (midP[0] - p1[0]))
                         ]
 
-
-        #flip means draw on other side of the line
+        #flip just means find the point on opposite side of the line
         else:
             newPoint = [(midP[0] + (midP[1] - p1[1])),
                         (midP[1] - (midP[0] - p1[0]))
                         ]
             
-        #iterate dragon curves using the new point
+        #recursively draw new dragon curves using the new point
         dragon(p1, newPoint, order - 1, False)
         dragon(newPoint, p2, order - 1, True)
         
     else:
-        #if the order is zero, just draw a line
+        #if the order is zero, just draw a line from p1 to p2
         pen.goto(p1)
         pen.pendown()
         pen.goto(p2)
@@ -57,4 +59,4 @@ wn.tracer(0,0) # <-- caption this if you want to see the line being drawn
 dragon([-200,-150], [200,150], 11, False)
 update() # <-- also caption this
 
-mainloop() #make the window not close immediately
+mainloop() #makes the window not close immediately
